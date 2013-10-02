17bf39c61b68f54b8b6b9544558b17fa3317061d
matYang <uwse@me.com>
2013-10-02 13:06:32 -0400

    adding getLocationRepresentation to location service

--- src/main/java/carpool/locationService/LocationService.java
+++ src/main/java/carpool/locationService/LocationService.java
@@ -25,8 +25,12 @@ public class LocationService {
 		lookupMap = CarpoolLocationLoader.getLookupMap();
 	}
 	
-	private static final CarpoolLocation getNode(String name, int depth){
-		return lookupMap.get(depth).get(name);
+	private static final CarpoolLocation getNode(int depth, String name) throws LocationException{
+		CarpoolLocation node;
+		if (depth >= lookupMap.size() || (node = lookupMap.get(depth).get(name)) == null){
+			throw new LocationException("Location not found");
+		}
+		return node;
 	}
 	
 	private static final ArrayList<CarpoolLocation> getAllNodesWithDepth(int depth){
@@ -58,7 +62,7 @@ public class LocationService {
 	
 	public static final ArrayList<String> getAllNamesWithDepth(int depth){
 		ArrayList<String> nameList = new ArrayList<String>();
-		for (CarpoolLocation loc : lookupMap.get(depth).values()){
+		for (CarpoolLocation loc :  getAllNodesWithDepth(depth)){
 			nameList.add(loc.getName());
 		}
 		return nameList;
@@ -66,10 +70,7 @@ public class LocationService {
 	
 	public static final ArrayList<String> getAllSubLocationNamesWithNode(int depth, String name)throws  LocationException{
 		ArrayList<String> nameList = new ArrayList<String>();
-		CarpoolLocation node;
-		if (depth >= lookupMap.size() || (node = lookupMap.get(depth).get(name)) == null){
-			throw new LocationException("Location invalid");
-		}
+		CarpoolLocation node = getNode(depth, name);
 		for (CarpoolLocation loc : node.getSubLocations().values()){
 			nameList.add(loc.getName());
 		}
@@ -77,16 +78,21 @@ public class LocationService {
 	}
 	
 	public static final JSONObject getSpecificJSONNode(int depth, String name) throws LocationException{
-		CarpoolLocation node;
-		if (depth >= lookupMap.size() || (node = lookupMap.get(depth).get(name)) == null){
-			throw new LocationException("Location invalid");
-		}
+		CarpoolLocation node = getNode(depth, name);
 		return node.toJSON();
 		
 	}
 	
 	public static final LocationRepresentation getLocationRepresentation(int depth, String name) throws LocationException{
-		//recursion
+		ArrayList<String> hierarchyNameList = new ArrayList<String>();
+		CarpoolLocation node = getNode(depth, name);
+		while (node !=  null){
+			hierarchyNameList.add(node.getName());
+			node = node.getParent();
+		}
+		Collections.reverse(hierarchyNameList);
+		return new LocationRepresentation(hierarchyNameList, CarpoolConfig.customDepth);
+		
 	}
 	
 	
