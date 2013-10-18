68b3288769598157ef81ea34eab59234469325f4
matYang <uwse@me.com>
2013-10-18 19:11:43 -0400

    Creating getTransactionByMessageId to subsitute the full message extraction mechanism

---
+++ src/main/java/carpool/resources/dianmingResource/DMTransctionResource.java
@@ -0,0 +1,63 @@
+package carpool.resources.dianmingResource;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import org.restlet.engine.header.Header;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.*;
+import org.restlet.util.Series;
+import org.restlet.data.*;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import carpool.dbservice.*;
+import carpool.exception.PseudoException;
+import carpool.factory.JSONFactory;
+import carpool.model.*;
+import carpool.resources.PseudoResource;
+
+
+public class DMTransactionResource extends PseudoResource{        
+
+    @Get
+    /**
+* @return the full transactions of the message
+*/
+    public Representation getTransactionByMessageId() {
+            int curMsgId = -1;
+            int curUserId = -1;
+        JSONArray response = new JSONArray();
+        
+        try {
+                        curMsgId = Integer.parseInt(this.getReqAttr("id"));
+                        curUserId = Integer.parseInt(this.getQueryVal("userId"));
+                        
+                        this.validateAuthentication(curUserId);
+                        
+                ArrayList<Transaction> historyTransactions = MessageDaoService.getTransactionByMessageId(curMsgId);
+                if (historyTransactions != null){
+                response = JSONFactory.toJSON(historyTransactions);
+                }
+                else{
+                        setStatus(Status.CLIENT_ERROR_CONFLICT);
+                }
+                        
+                } catch (PseudoException e){
+                        this.addCORSHeader();
+                        return new StringRepresentation(this.doPseudoException(e));
+                } catch (Exception e) {
+                        this.doException(e);
+                }
+        
+        
+        Representation result = new JsonRepresentation(response);
+        this.addCORSHeader();
+        return result;
+    }
+
+}
